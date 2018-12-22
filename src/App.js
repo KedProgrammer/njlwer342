3621c3dbefee6a600b329d7f143aa478fa725048
Janior <janior_orlando@hotmail.com>
2018-12-22 08:05:15 -0800

    reto React Tareas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
-
 /// Modifica el componente para que se puedan agregar tareas
-
 class App extends Component {
+  ///  inicio codigo adicionado
+  state = { 
+    task: ["Sacar la ropa", "Hacer la cama", "Leer un rato"], 
+    newTask: [] 
+  }; 
+  HandleInput = this.HandleInput.bind(this); 
+  HandleKey = this.HandleKey.bind(this); 
+  HandleInput(e) { 
+     this.setState({ 
+       newTask: e.target.value 
+     }); 
+   } 
+   HandleKey(ev) { 
+     ev.preventDefault(); 
+     this.setState({ 
+       task: this.state.task.concat(this.state.newTask), 
+       newTask: "" 
+     }); 
+   } 
+  ///  fin codigo adicionado
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+  
+          ///  inicio codigo adicionado
+          <h2>Cosas</h2> 
+           <ul className="todo"> 
+             {this.state.task.map((tasks, index) => ( 
+               <li key={index}>{tasks}</li> 
+             ))} 
+           </ul> 
+
+           <form onSubmit={this.HandleKey}> 
+             <input 
+               type="text" 
+               id="new-task" 
+               value={this.state.newTask} 
+               placeholder="Ingresa una tarea y oprime Enter" 
+               onChange={this.HandleInput} 
+             /> 
+           </form> 
+          ///  fin codigo adicionado
         </div>
       </div>
     )
   }
 }
-
-
 export default App;
