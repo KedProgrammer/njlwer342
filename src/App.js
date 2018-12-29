f384f71bb74ad832e985bd0a8001f2efdb3763d4
Janior <janior_orlando@hotmail.com>
2018-12-29 09:03:17 -0800

    Validando error delk test

--- src/App.js
+++ src/App.js
@@ -1,54 +1,47 @@
-import React, { Component } from 'react';
-/// Modifica el componente para que se puedan agregar tareas
-class App extends Component {
-  ///  inicio codigo adicionado
-  state = { 
-    task: ["Sacar la ropa", "Hacer la cama", "Leer un rato"], 
-    newTask: [] 
-  }; 
-  HandleInput = this.HandleInput.bind(this); 
-  HandleKey = this.HandleKey.bind(this); 
-  HandleInput(e) { 
-     this.setState({ 
-       newTask: e.target.value 
-     }); 
+import React, { Component } from 'react'; 
+/// Modifica el componente para que se puedan agregar tareas 
+ 
+class App extends Component { 
+  constructor() { 
+    super() 
+    this.state = { 
+      tasks: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato'], 
+      newTask: "" 
+     } 
    } 
-   HandleKey(ev) { 
-     ev.preventDefault(); 
-     this.setState({ 
-       task: this.state.task.concat(this.state.newTask), 
-       newTask: "" 
-     }); 
-   } 
-  ///  fin codigo adicionado
-
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-  
-          ///  inicio codigo adicionado
-          <h2>Cosas</h2> 
+ 
+   render() { 
+     return ( 
+       <div className="wrapper"> 
+         <div className="list"> 
+           <h3>Por hacer:</h3> 
            <ul className="todo"> 
-             {this.state.task.map((tasks, index) => ( 
-               <li key={index}>{tasks}</li> 
-             ))} 
+               {this.state.tasks.map((LaTarea, index) => 
+                 <li key={index}>{LaTarea}</li> 
+               )} 
            </ul> 
+              <form onSubmit={this.handleSubmit}> 
+                <input type="text" id="new-task" value={this.state.newTask} onChange={this.addNewTask.bind(this)} placeholder="Ingresa una tarea y oprime Enter" /> 
+              </form> 
+         </div> 
+       </div> 
+     ) 
+   }
 
-           <form onSubmit={this.HandleKey}> 
-             <input 
-               type="text" 
-               id="new-task" 
-               value={this.state.newTask} 
-               placeholder="Ingresa una tarea y oprime Enter" 
-               onChange={this.HandleInput} 
-             /> 
-           </form> 
-          ///  fin codigo adicionado
-        </div>
-      </div>
-    )
-  }
-}
-export default App;
+   addNewTask (event) { 
+     this.setState({ 
+       newTask: event.target.value 
+     }) 
+   } 
+
+   handleSubmit = (e) => { 
+     e.preventDefault() 
+     let oldTasks = this.state.tasks 
+     let newTask = this.state.newTask 
+     this.setState({ 
+       tasks: [...oldTasks, newTask], 
+       newTask: '' 
+     }) 
+   } 
+ } 
+export default App; 
